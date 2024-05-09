c2687574cea6f3be42a502a299f618278fce3e20
immerfallen <marodemelo10@gmail.com>
2024-05-09 18:18:46 -0300

    criado componente de filtro

--- Secao-5-ProjetoFiltroListaUsuarios/FiltroUsuarios/src/app/angular-material/angular-material.module.ts
+++ Secao-5-ProjetoFiltroListaUsuarios/FiltroUsuarios/src/app/angular-material/angular-material.module.ts
@@ -2,15 +2,35 @@ import { NgModule } from "@angular/core";
 
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider'
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
     imports: [
         MatListModule,
-        MatDividerModule
+        MatDividerModule,
+        MatFormFieldModule, 
+        MatInputModule,
+        MatDatepickerModule,
+        MatSelectModule,
+        MatButtonModule,        
     ],
     exports: [
         MatListModule,
-        MatDividerModule
+        MatDividerModule,
+        MatFormFieldModule, 
+        MatInputModule,
+        MatDatepickerModule,
+        MatSelectModule,
+        MatButtonModule,        
     ],
+    providers: [
+        provideNativeDateAdapter()
+    ]
 })
 
 export class AngularMaterialModule {
---
+++ Secao-5-ProjetoFiltroListaUsuarios/FiltroUsuarios/src/app/components/filter/filter.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.component.html',
+  styleUrl: './filter.component.scss',
+})
+export class FilterComponent {
+  options = [
+    {
+      value: true,
+      viewValue: 'Ativo',
+    },
+    {
+      value: false,
+      viewValue: 'Inativo',
+    },
+  ];
+}
