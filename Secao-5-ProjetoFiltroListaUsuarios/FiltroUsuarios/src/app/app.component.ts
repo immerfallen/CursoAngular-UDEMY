d21d18e8123fb3bdd99f5c05209ec7aa510b355b
immerfallen <marodemelo10@gmail.com>
2024-05-10 16:53:54 -0300

    Criado filtro para nomes

--- Secao-5-ProjetoFiltroListaUsuarios/FiltroUsuarios/src/app/app.component.ts
+++ Secao-5-ProjetoFiltroListaUsuarios/FiltroUsuarios/src/app/app.component.ts
@@ -6,31 +6,50 @@ import { IFilterOptions } from './interfaces/filter/filter-options.interface';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-
   showHeaderTable: boolean = false;
 
   ngOnInit() {
     setTimeout(() => {
       this.usersList = UsersList;
+      this.usersListFiltered = this.usersList;
       this.showHeaderTable = true;
-    }, 3);;
+    }, 3);
   }
 
   usersList: IUser[] = [];
+  usersListFiltered: IUser[] = [];
   userSelected: IUser = {} as IUser;
-  showUserDetails = false;  
+  showUserDetails = false;
 
-  onUserSelected(user:IUser){    
+  onUserSelected(user: IUser) {
     this.userSelected = user;
     this.showUserDetails = true;
   }
 
-  onFilter(filter: IFilterOptions){
-    console.log(filter);
+  onFilter(filter: IFilterOptions) {
+    this.usersListFiltered = this.filterUsersList(filter, this.usersList);
   }
 
+  filterUsersList(filter: IFilterOptions, list: IUser[]): IUser[]{
+    let filteredList: IUser[] = [];
+filteredList = this.filterUsersListByName(filter.name, list);
+    return filteredList;
   }
 
+  filterUsersListByName(name: string | undefined, list: IUser[]): IUser[]  {
+    const NAME_NOT_TYPPED = name == undefined;
+
+    if(NAME_NOT_TYPPED){
+      return list;
+    }
+
+    const filteredList = list.filter((user)=> user.nome.toLowerCase().includes(name.toLowerCase()));
+
+    return filteredList;
+  }
+
+  
+}
