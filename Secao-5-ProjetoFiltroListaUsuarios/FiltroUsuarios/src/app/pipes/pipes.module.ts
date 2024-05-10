2b526d48b51851b287a8ea13c5fb43a1874b797f
immerfallen <marodemelo10@gmail.com>
2024-05-10 10:40:51 -0300

    criado pipe para status

--- Secao-5-ProjetoFiltroListaUsuarios/FiltroUsuarios/src/app/pipes/pipes.module.ts
+++ Secao-5-ProjetoFiltroListaUsuarios/FiltroUsuarios/src/app/pipes/pipes.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PhonePipe } from './phone.pipe';
 import { AddressPipe } from './address.pipe';
+import { StatusPipe } from './status.pipe';
 
 @NgModule({
-  exports: [PhonePipe, AddressPipe],  
-  declarations: [PhonePipe, AddressPipe],
+  exports: [PhonePipe, AddressPipe, StatusPipe],  
+  declarations: [PhonePipe, AddressPipe, StatusPipe],
 })
 export class PipesModule {}
