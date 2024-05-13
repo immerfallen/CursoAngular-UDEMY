f04bf8bf2579ba9be30f0aa0305902b4fdd462bc
immerfallen <marodemelo10@gmail.com>
2024-05-13 08:53:22 -0300

    criado filtro para datas

--- Secao-5-ProjetoFiltroListaUsuarios/FiltroUsuarios/src/app/app.component.ts
+++ Secao-5-ProjetoFiltroListaUsuarios/FiltroUsuarios/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
 import { UsersList } from './data/users.list';
 import { IFilterOptions } from './interfaces/filter/filter-options.interface';
+import { isWithinInterval } from 'date-fns';
 
 @Component({
   selector: 'app-root',
@@ -37,6 +38,7 @@ export class AppComponent implements OnInit {
     let filteredList: IUser[] = [];
     filteredList = this.filterUsersListByName(filter.name, list);
     filteredList = this.filterUsersListByStatus(filter.status, filteredList);
+    filteredList = this.filterUsersListByDate(filter.startDate, filter.endDate, filteredList);
     return filteredList;
   }
 
@@ -48,6 +50,19 @@ export class AppComponent implements OnInit {
     const filteredList = list.filter((user) => user.ativo === status);
 
     return filteredList;
+  }
+
+  filterUsersListByDate(startDate: Date | undefined, endDate: Date | undefined, list: IUser[]) : IUser[]{
+    const DATES_NOT_SELECTED = startDate === undefined || endDate == undefined;
+
+    if(DATES_NOT_SELECTED){
+      return list;
+    }
+
+    const listFiltered = list.filter((user)=> isWithinInterval(new Date(user.dataCadastro), 
+    {start: startDate, end: endDate}));
+
+    return listFiltered;
   }
 
   filterUsersListByName(name: string | undefined, list: IUser[]): IUser[] {
